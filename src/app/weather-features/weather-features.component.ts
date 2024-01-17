cb8dfa1c744072d7d0f13ab34aa6251174d5beec
Prince Mpho Msimango <144108667+YungPharaoh65@users.noreply.github.com>
2024-01-17 10:56:36 +0200

    Update weather-features.component.ts
    Rewrote gif to display

--- src/app/weather-features/weather-features.component.ts
+++ src/app/weather-features/weather-features.component.ts
@@ -8,14 +8,12 @@ import { NgxSpinnerService } from 'ngx-spinner';
   styleUrls: ['./weather-features.component.css'],
 })
 export class WeatherFeaturesComponent {
+  
   gifUrl: SafeResourceUrl;
 
-  constructor(
-    private sanitizer: DomSanitizer,
-    private spinner: NgxSpinnerService
-  ) {
+  constructor(private sanitizer: DomSanitizer,
+    private spinner: NgxSpinnerService) {
     // Replace 'assets/giphy.gif' with the actual path to your GIF file.
-    this.gifUrl =
-      this.sanitizer.bypassSecurityTrustResourceUrl('/assets/rain.gif'); //this is the gif img function
+    this.gifUrl = this.sanitizer.bypassSecurityTrustResourceUrl('assets/rain.gif'); //this is the gif img function
   }
 }
